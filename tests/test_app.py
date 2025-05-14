7b7a3d89888767b4b7f6d5c4d36fafb53ac447a8
Allan Oliveira <allan.sbo@hotmail.com>
2025-05-14 19:27:18 -0300

    Exercicio aula 2

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,6 +8,13 @@ client = TestClient(app)
 
 
 def test_root_must_return_hello_world():
-    response = client.get("/")
+    response = client.get('/')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Hello World'}
+
+
+def test_exercicio_aula_02():
+    response = client.get('exercicio-html')
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo </h1>' in response.text
+    assert response.headers['Content-Type'] == 'text/html; charset=utf-8'
